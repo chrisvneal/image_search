3cca972b959842ba389d0ac943b22e07276d1a6e
Chris Neal <chrisvneal@gmail.com>
2022-03-27 01:52:40 -1000

    Create onSubmitSearch function in App component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
